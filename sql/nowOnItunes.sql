7759e95486bdd045f8d7b66e82cc4ce0b8a643fa
XPac <xpac27@gmail.com>
2010-11-26 14:29:12 +0100

    New sql template

--- sql/nowOnItunes.sql
+++ sql/nowOnItunes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.4
+-- version 3.3.7deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2010 at 11:58 PM
--- Server version: 5.1.47
--- PHP Version: 5.3.3
+-- Generation Time: Nov 26, 2010 at 02:24 PM
+-- Server version: 5.1.49
+-- PHP Version: 5.3.3-1ubuntu9.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -16,7 +16,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `nowOnItune`
+-- Database: `nowonitunes`
 --
 
 -- --------------------------------------------------------
@@ -27,10 +27,15 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `band` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `official` tinyint(1) NOT NULL DEFAULT '0',
+  `public` tinyint(1) NOT NULL DEFAULT '0',
+  `ip` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `homepage` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `creation_date` int(11) NOT NULL,
   `view_date` int(11) NOT NULL,
   `view_count` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=46 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3190 ;
